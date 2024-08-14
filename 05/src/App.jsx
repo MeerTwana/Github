f727bf4d396f7feb3a3553efbc65f6221ff48483
Meer <meer.00917291@gmail.com>
2024-08-14 11:46:49 +0300

    some modification to the code

--- 05/src/App.jsx
+++ 05/src/App.jsx
@@ -4,7 +4,11 @@ import Header from './components/Header.jsx';
 export default function App() {
   return (
     <>
-      <Header />
+   
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  <Header />
       <main>
         <AuthInputs />
       </main>
