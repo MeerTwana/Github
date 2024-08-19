8aa9506cbe5066de2a08898f46191368d4a4eff9
Meer <meer.00917291@gmail.com>
2024-08-19 11:30:48 +0300

    07 added

---
+++ 07/src/App.jsx
@@ -0,0 +1,12 @@
+import SideBar from "../Components/SideBar";
+function App() {
+  return (
+    
+    <main className="h-screen my-8 flex gap-8">
+   <SideBar/>
+
+  </main> 
+  );
+}
+
+export default App;
