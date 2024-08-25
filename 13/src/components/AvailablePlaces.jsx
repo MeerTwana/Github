8dfd75f8e8de65984a63360b8d2024acd0737aa9
Meer <meer.00917291@gmail.com>
2024-08-25 16:16:51 +0300

    some edit

---
+++ 13/src/components/AvailablePlaces.jsx
@@ -0,0 +1,31 @@
+import Places from './Places.jsx';
+import { useEffect, useState } from 'react';
+
+export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]); // Initialize as an empty array
+
+  useEffect(() => {
+    fetch('http://localhost:3000/places')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Server response was not sent');
+        }
+        return response.json();
+      })
+      .then((resData) => {
+        setAvailablePlaces(resData.places);
+      })
+      .catch((error) => {
+        console.error('Error fetching places:', error);
+      });
+  }, []); 
+
+  return (
+    <Places
+      title="Available Places"
+      places={availablePlaces}
+      fallbackText="No places available."
+      onSelectPlace={onSelectPlace}
+    />
+  );
+}
