dce2e9c48ff7a19beeccbf6875ba97b2d5b5e5e7
Meer <meer.00917291@gmail.com>
2024-08-28 17:05:47 +0300

    task reminder added some them

---
+++ TaskRmeinder/src/App.jsx
@@ -0,0 +1,12 @@
+import CreateReminder from "./components/CreateReminder"
+
+export default function App() {
+  return (
+   <div className="w-2/4  h-96 ml-10 mt-10 bg-gray-200 ">
+<h1 className="text-4xl pl-1">Reminder</h1>
+
+<CreateReminder></CreateReminder>
+
+   </div>
+  )
+}
---
+++ TaskRmeinder/src/components/CreateReminder.jsx
@@ -0,0 +1,64 @@
+import { Select } from 'antd';
+import { Calendar, theme } from 'antd';
+
+const onPanelChange = (value, mode) => {
+  console.log(value.format('YYYY-MM-DD'), mode);
+};
+
+export default function CreateReminder(){
+  const { token } = theme.useToken();
+  const wrapperStyle = {
+    width: 300,
+    border: `1px solid ${token.colorBorderSecondary}`,
+    borderRadius: token.borderRadiusLG,};
+
+    return(
+<div className="flex flex-col gap-6 p-4">
+<div className="  gap-6 pt-2 pb-4">
+  <p className="text-lg">Title :</p>
+  <input type="text" className="w-3/4 rounded-md h-8" />
+</div>
+
+<div className="  gap-6 pt-2 pb-4 ">
+  <p className="text-lg">Description :</p>
+  <textarea type="text" className="w-3/4 rounded-md h-20" />
+</div>
+<div className="  gap-6 pt-2 pb-4  flex flex-row">
+  <p className="text-lg">priority:
+  <Select className='w-24'
+    showSearch
+    placeholder="Select Priority"
+    filterOption={(input, option) =>
+      (option?.label ?? '').toLowerCase().includes(input.toLowerCase())
+    }
+    options={[
+        {
+            value:'0',
+            label:'None',
+        },
+      {
+        value: '1',
+        label: 'Low',
+      },
+      {
+        value: '2',
+        label: 'Medium',
+      },
+      {
+        value: '3',
+        label: 'High',
+      },
+    ]}
+  />
+  </p>
+  <p>Date : </p>
+  <div style={wrapperStyle}>
+      <Calendar fullscreen={false} onPanelChange={onPanelChange} />
+    </div>
+</div>
+ 
+</div>
+
+    );
+    
+}
